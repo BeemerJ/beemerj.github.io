cfb0aae176225b94b1b8c2f2d67c893d5f36613d
C̸̯̭̈́̓̿̄̆͠ͅo̴̭̣̩͌̓̎̉͐͂̈́̀͝d̸̡̗̀̇͝ȩ̷͕̲͔̗̤̰͆̏-̵̧̫̰̰̠̾̽͆̌͜M̸̬̹͚̠̖͖̻̾o̵̡̢̘̯͚̯̤͎̺͊̑̈́̂̒̐̎̓s̸̯̬̀͌̔̊͒̓̈́̕͝͝s̶̱̺̏̽̋̑̿ <74435632+Code-Moss@users.noreply.github.com>
2024-03-16 02:56:48 +0200

    made images pull from steamgrid.db

--- js/games.js
+++ js/games.js
@@ -1,27 +1,30 @@
-        // Array of game objects
-        const games = [
-            { name: "Postal", image: "../assets/images/covers/postal.jpg" },
-            { name: "Postal 2", image: "../assets/images/covers/postal2.jpg" },
-            { name: "Postal: Brain Damaged", image: "../assets/images/covers/postal_bd.jpg" },
-        ];
+// Add games to this variable
+const games = [
+    { done: false, name: "Postal", image: "https://cdn2.steamgriddb.com/thumb/05b5f43841bf592510e90dffa4bf2177.jpg" },
+    { done: false, name: "Postal 2", image: "https://cdn2.steamgriddb.com/thumb/e568b8f6bbb04a676a4f9f7dd79dc79f.jpg" },
+    { done: false, name: "Postal: Brain Damaged", image: "https://cdn2.steamgriddb.com/thumb/a857ff5359cbc948ae2b81468839d718.jpg" },
+];
 
-        // Function to generate HTML for each game
-        function generateGames() {
-            const contentDiv = document.getElementById('content');
-            games.forEach(game => {
-                const gameDiv = document.createElement('div');
-                gameDiv.classList.add('game');
-                const gameImage = document.createElement('img');
-                gameImage.src = game.image;
-                gameImage.alt = game.name;
-                const gameName = document.createElement('span');
-                gameName.textContent = game.name;
-                gameName.classList.add('gameName'); // Adding class for styling
-                gameDiv.appendChild(gameImage);
-                gameDiv.appendChild(gameName);
-                contentDiv.appendChild(gameDiv);
-            });
+function generateGames() {
+    const contentDiv = document.getElementById('content');
+    games.forEach(game => {
+        const gameDiv = document.createElement('div');
+        gameDiv.classList.add('game');
+        
+        const gameImage = document.createElement('img');
+        gameImage.src = game.image;
+        gameImage.alt = game.name; // Set alt text to game name
+        
+        // Add styling if done = true
+        if (game.done) {
+            gameImage.classList.add('doneEffect');
         }
 
-        // Call the function to generate games when the page loads
-        window.onload = generateGames;
+        gameDiv.appendChild(gameImage);
+        contentDiv.appendChild(gameDiv);
+    });
+}
+
+
+// Call the function to generate games when the page loads
+window.onload = generateGames;
